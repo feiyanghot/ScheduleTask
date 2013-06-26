9dc900aff93eba953ce63f1b94354bc883d2b0f0
Mohan Kumar V <mohan21_kumar@yahoo.co.in>
2013-06-26 18:37:04 +0530

    Schedule Task

---
+++ SchedulerTask/SchedulerTask/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Quartz;
+using Quartz.Impl;
+using log4net;
+
+namespace SchedulerTask
+{
+    class Program
+    {
+        private static readonly ILog logger = LogManager.GetLogger(typeof(Program));
+
+        static void Main(string[] args)
+        {
+            logger.Info("Starting the Scheduler");
+            var schedulerFactory = new StdSchedulerFactory(); 
+            var scheduler = schedulerFactory.GetScheduler(); 
+            if(!scheduler.IsStarted)
+                scheduler.Start();
+            logger.Info("Scheduler Started");
+
+            var job = JobBuilder.Create<SampleJob>().Build();
+
+            var trigger = TriggerBuilder.Create()
+                .WithSimpleSchedule(x => x.WithIntervalInSeconds(15).RepeatForever())
+                .Build();
+
+            scheduler.ScheduleJob(job, trigger);
+            logger.Info("Scheduler Scheduled");
+        }
+    }
+}
---
+++ SchedulerTask/SchedulerTask/SampleJob.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Quartz;
+using System.Net;
+using log4net;
+
+namespace SchedulerTask
+{
+    public class SampleJob : IJob
+    {
+        private EloquaService.EloquaServiceClient serviceProxy;
+
+        private EloquaExternalActionService.ExternalActionServiceClient programServiceProxy;
+
+        private DateTime dttLastEloquaAPICall;
+
+        private static readonly ILog logger = LogManager.GetLogger(typeof(SampleJob));
+
+        public void Execute(IJobExecutionContext context)
+        {
+            logger.Debug("Scheduler Executed : " + DateTimeOffset.Now);
+            //Console.WriteLine(DateTimeOffset.Now); 
+        }
+    }
+}
